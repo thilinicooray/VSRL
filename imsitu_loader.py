b5fa9d9540a2cdc7a8950c08ed434573188c631c
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-07-21 18:54:23 +0800

    replace resize

--- imsitu_loader.py
+++ imsitu_loader.py
@@ -47,7 +47,7 @@ class imsitu_loader(data.Dataset):
         #im_in = np.array(Image.open(os.path.join(self.img_dir, _id)).convert('RGB'))
         im_in = Image.open(os.path.join(self.img_dir, _id)).convert('RGB')
         transformed = self.transform(im_in)
-        print('transformed image :', transformed)
+        print('transformed image :', transformed.size())
         '''im_file = os.path.join(self.img_dir, _id)
         # im = cv2.imread(im_file)
         im_in = np.array(imread(im_file))
