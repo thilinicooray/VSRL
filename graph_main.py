c8bbe25d93933964888c35a564588d6bee38876a
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-07-24 00:52:17 +0800

    bug fix

--- graph_main.py
+++ graph_main.py
@@ -12,7 +12,7 @@ def train(model, train_loader, dev_loader, optimizer, max_epoch, model_dir, enco
     train_loss = 0
     total_steps = 0
     print_freq = 3200
-    dev_loss_list = []
+    dev_score_list = []
 
     top1 = imsitu_scorer(encoder, 1, 3)
     top5 = imsitu_scorer(encoder, 5, 3)
@@ -97,7 +97,7 @@ def train(model, train_loader, dev_loader, optimizer, max_epoch, model_dir, enco
 
 
             if total_steps % eval_frequency == 0:
-                top1, top5, dev_loss = eval(model, dev_loader, encoder, gpu_mode)
+                top1, top5 = eval(model, dev_loader, encoder, gpu_mode)
                 model.train()
 
                 top1_avg = top1.get_average_results()
@@ -111,22 +111,23 @@ def train(model, train_loader, dev_loader, optimizer, max_epoch, model_dir, enco
                                                              utils.format_dict(top1_avg,'{:.2f}', '1-'),
                                                              utils.format_dict(top5_avg, '{:.2f}', '5-')))
 
-                dev_loss_list.append(dev_loss)
-                min_loss = min(dev_loss_list)
+                dev_score_list.append(avg_score)
+                max_score = max(dev_score_list)
 
-                if min_loss == dev_loss_list[-1]:
-                    checkpoint_name = os.path.join(model_dir, '{}_devloss_{}.h5'.format('baseline', len(dev_loss_list)))
+                if max_score == dev_score_list[-1]:
+                    checkpoint_name = os.path.join(model_dir, '{}_devloss_{}.h5'.format('baseline', len(dev_score_list)))
                     utils.save_net(checkpoint_name, model)
-                    print ('New best model saved! {0}'.format(min_loss))
+                    print ('New best model saved! {0}'.format(max_score))
 
                 print('current train loss', train_loss)
                 train_loss = 0
+                top1 = imsitu_scorer(encoder, 1, 3)
+                top5 = imsitu_scorer(encoder, 5, 3)
 
         print('Epoch ', epoch, ' completed!')
 
 def eval(model, dev_loader, encoder, gpu_mode):
     model.eval()
-    dev_loss = 0
 
     print ('evaluating model...')
     top1 = imsitu_scorer(encoder, 1, 3)
@@ -161,15 +162,12 @@ def eval(model, dev_loader, encoder, gpu_mode):
 
         verb_predict, role_predict = model(im_data, im_info, gt_boxes, num_boxes, verb, roles)
 
-        loss = model.calculate_loss(verb_predict, verb, role_predict, labels)
-        dev_loss += loss.data
-
         top1.add_point(torch.unsqueeze(verb_predict,0), torch.unsqueeze(verb,0),
                        torch.unsqueeze(role_predict,0), torch.unsqueeze(labels,0))
         top5.add_point(torch.unsqueeze(verb_predict,0), torch.unsqueeze(verb,0),
                        torch.unsqueeze(role_predict,0), torch.unsqueeze(labels,0))
 
-    return top1, top5, dev_loss
+    return top1, top5
 
 def main():
 
