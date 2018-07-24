89fe86ef8141b7568b0074fcf3d0b061868b0c5e
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-07-24 14:42:27 +0800

    tensor expansion

--- models/graph_baseline.py
+++ models/graph_baseline.py
@@ -125,19 +125,18 @@ class baseline(nn.Module):
         #original code use gold verbs to insert to role predict module (only at training )
 
         role_embedding = self.role_lookup_table(roles)
-        role_embedding = torch.unsqueeze(role_embedding,3)
         print('role_embedding :', role_embedding.size())
 
         role_label_embd_list = []
 
         #for attention, first try with node only
         #todo: use edge for this calculation
-        role_expanded_state = role_embedding.expand(role_embedding.size(0), role_embedding.size(1), role_embedding.size(2),
-                                                    edge_states.size(1))
-        role_expanded_state = role_expanded_state.transpose(2,3)
-        vert_state_expanded = vert_states.expand(vert_states.size(0), role_embedding.size(1),role_embedding.size(2),
-                                                 vert_states.size(1))
-        vert_state_expanded = vert_state_expanded.transpose(2,3)
+        role_expanded_state = role_embedding.expand(edge_states.size(1),role_embedding.size(0), role_embedding.size(1),
+                                                    role_embedding.size(2))
+        role_expanded_state = role_expanded_state.permute(1,2,0,3)
+        vert_state_expanded = vert_states.expand(role_embedding.size(1),vert_states.size(0), role_embedding.size(1),
+                                                 vert_states.size(2))
+        vert_state_expanded = vert_state_expanded.transpose(0,1)
         print('expand :', role_expanded_state.size(), vert_state_expanded.size())
         role_concat = torch.cat((role_expanded_state, vert_state_expanded[:,:,1:]), 3)
         print('cat :', role_concat.size())
