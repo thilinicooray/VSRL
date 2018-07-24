ab5c207bd776efd24c4b855327a2581cc020e858
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-07-24 13:59:42 +0800

    tensor expansion

--- models/action_graph.py
+++ models/action_graph.py
@@ -23,13 +23,13 @@ class action_graph(nn.Module):
 
         self.edge_att = nn.Sequential(
             nn.Linear(self.edge_state_dim * 2, 1),
-            nn.Tanh(),
+            nn.Sigmoid(),
             nn.LogSoftmax()
         )
 
         self.vert_att = nn.Sequential(
             nn.Linear(self.vert_state_dim * 2, 1),
-            nn.Tanh(),
+            nn.Sigmoid(),
             nn.LogSoftmax()
         )
 
@@ -40,25 +40,34 @@ class action_graph(nn.Module):
     def forward(self, input):
 
         #init hidden state with zeros
-        vert_state = torch.zeros(input[0].size(0), input[0].size(1), self.vert_state_dim)
-        edge_state = torch.zeros(input[1].size(0),input[1].size(1), self.edge_state_dim)
+        vert_state = torch.zeros(input[0].size(1), self.vert_state_dim)
+        edge_state = torch.zeros(input[1].size(1), self.edge_state_dim)
 
         if self.gpu_mode >= 0:
             vert_state = vert_state.to(torch.device('cuda'))
             edge_state = edge_state.to(torch.device('cuda'))
 
-        vert_state = self.vert_gru(input[0], vert_state)
-        edge_state = self.edge_gru(input[1], edge_state)
+        batch_size = input[0].size(0)
+        vert_state_list, edge_state_list = []
+        #todo: can this be parallelized?
+        for i in range(batch_size):
+            input = input[i]
+            vert_state.fill_(0)
+            vert_state.fill_(0)
+            vert_state = self.vert_gru(input[0], vert_state)
+            edge_state = self.edge_gru(input[1], edge_state)
 
-        #todo: check whether this way is correct, TF code uses a separate global var to keep hidden state
-        for i in range(self.num_steps):
-            edge_context = self.get_edge_context(edge_state, vert_state)
-            vert_context = self.get_vert_context(vert_state, edge_state)
+            #todo: check whether this way is correct, TF code uses a separate global var to keep hidden state
+            for i in range(self.num_steps):
+                edge_context = self.get_edge_context(edge_state, vert_state)
+                vert_context = self.get_vert_context(vert_state, edge_state)
 
-            edge_state = self.edge_gru(edge_context, edge_state)
-            vert_state = self.vert_gru(vert_context, vert_state)
+                edge_state = self.edge_gru(edge_context, edge_state)
+                vert_state = self.vert_gru(vert_context, vert_state)
 
-        return vert_state, edge_state
+            vert_state_list.append(vert_state), edge_state_list.append(edge_state)
+
+        return torch.stack(vert_state_list), torch.stack(edge_state)
 
     def get_edge_context(self, edge_state, vert_state):
         #todo: implement for undirectional, not only have verb-> region direction.
@@ -71,15 +80,14 @@ class action_graph(nn.Module):
         :return:
         '''
 
-        verb_vert_state = vert_state[:,0]
-        region_vert_state = vert_state[:,1:]
-        verb_expanded_state = verb_vert_state.expand(region_vert_state.size(1),verb_vert_state.size(0), verb_vert_state.size(1))
-        verb_expanded_state = verb_expanded_state.transpose(0,1)
+        verb_vert_state = vert_state[0]
+        region_vert_state = vert_state[1:]
+        verb_expanded_state = verb_vert_state.expand(region_vert_state.size(0), verb_vert_state.size(0))
 
-        print('vert shapes', verb_vert_state.size(), region_vert_state.size(), verb_expanded_state.size())
+        #print('vert shapes', verb_vert_state.size(), region_vert_state.size(), verb_expanded_state.size())
 
-        verb_concat = torch.cat((verb_expanded_state, edge_state), 2)
-        region_concat = torch.cat((region_vert_state, edge_state), 2)
+        verb_concat = torch.cat((verb_expanded_state, edge_state), 1)
+        region_concat = torch.cat((region_vert_state, edge_state), 1)
 
         att_weighted_verb = torch.mul(self.edge_att(verb_concat), verb_expanded_state)
         att_weighted_region = torch.mul(self.edge_att(region_concat), region_vert_state)
@@ -87,42 +95,20 @@ class action_graph(nn.Module):
         return att_weighted_verb + att_weighted_region
 
     def get_vert_context(self, vert_state, edge_state):
-        verb_vert_state = vert_state[:,0]
-        region_vert_state = vert_state[:,1:]
-        verb_expanded_state = verb_vert_state.expand(region_vert_state.size(1),verb_vert_state.size(0), verb_vert_state.size(1))
-        verb_expanded_state = verb_expanded_state.transpose(0,1)
+        verb_vert_state = vert_state[0]
+        region_vert_state = vert_state[1:]
+        verb_expanded_state = verb_vert_state.expand(region_vert_state.size(0), verb_vert_state.size(0))
 
         #print('vert shapes', verb_vert_state.size(), region_vert_state.size(), verb_expanded_state.size())
 
-        verb_concat = torch.cat((verb_expanded_state, edge_state), 2)
-        region_concat = torch.cat((region_vert_state, edge_state), 2)
+        verb_concat = torch.cat((verb_expanded_state, edge_state), 1)
+        region_concat = torch.cat((region_vert_state, edge_state), 1)
 
         att_weighted_verb_per_edge = torch.mul(self.vert_att(verb_concat), edge_state)
         att_weighted_region = torch.mul(self.edge_att(region_concat), edge_state)
-        att_weighted_verb = torch.sum(att_weighted_verb_per_edge, 1)
+        att_weighted_verb = torch.sum(att_weighted_verb_per_edge, 0)
 
-        vert_ctx = torch.cat((att_weighted_verb,att_weighted_region), 1)
+        vert_ctx = torch.cat((torch.unsqueeze(att_weighted_verb,0),att_weighted_region), 0)
 
-        print('vert context :', vert_ctx.size())
+        #print('vert context :', vert_ctx.size())
         return vert_ctx
-
-
-    '''
-    in graph_baseline
-    init
-    cnn
-    linear layer for 1024->512 (or update faster rcnn itself
-    action_graph
-    verb_layer
-    attention layer
-    role_layer
-    
-    forward
-    get region x features from faster rcnn
-    get vert_date, edge_state from action graph
-    get verb softmax from verb_layer
-    get role based attention for 6 roles
-    get label_rep using attention
-    get label softmax using role layer
-    
-    '''
